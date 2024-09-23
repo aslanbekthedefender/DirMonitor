fe0583eb0cd778d311af5393ecd7dec9b57863af
Aslanbek <37618559+aslanbekthedefender@users.noreply.github.com>
2024-09-23 21:50:41 +0400

    Delete DirMonitor-main/DirMonitor directory

--- DirMonitor-main/DirMonitor/Source/config.py
+++
@@ -1,6 +0,0 @@
-# Configuration settings for the tool
-LOG_FILE_REGISTRY = "registry_changes.log"
-LOG_FILE_DIRECTORY = "file_changes.log"
-LOG_FILE_NETWORK = "network_activity.log"
-ALLOW_USER_INPUT = True
-TRACK_DISK_CHANGES = True
